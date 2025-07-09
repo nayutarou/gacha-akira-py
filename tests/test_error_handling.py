b869961043d261d21bedbd1ec515a78afc134c5c
akira <a.oono.sys23@morijyobi.ac.jp>
2025-07-09 10:13:41 +0900

    テスト結果の出力

--- tests/test_error_handling.py
+++ tests/test_error_handling.py
@@ -8,12 +8,25 @@ from gacha import gacha_draw
 
 
 def test_return_type():
-    result = gacha_draw()
-    assert isinstance(result, str)
-    assert len(result) == 1
+    print("\n[エラーテスト] 返り値の型")
+    try:
+        result = gacha_draw()
+        assert isinstance(result, str)
+        assert len(result) == 1
+        print("[テスト成功] 返り値の型が正しく出現します")
+    except AssertionError:
+        print("[テスト失敗] 返り値の型が正しく出現しません")
+        raise
+    
 
 
 def test_only_valid_items():
-    valid = {"A", "B", "C", "D"}
-    for _ in range(10000):
-        assert gacha_draw() in valid
+    print("\n[エラーテスト] 有効なアイテムしか出ない")
+    try:        
+        valid = {"A", "B", "C", "D"}
+        for _ in range(10000):
+            assert gacha_draw() in valid
+        print("[テスト成功] 有効なアイテムしか出ない")
+    except:
+        print("[テスト失敗] 有効なアイテムしか出ない")
+        raise
