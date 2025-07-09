f13794524f2de0b01e8e39ea3260a04283613548
akira <a.oono.sys23@morijyobi.ac.jp>
2025-07-09 09:48:32 +0900

    結果だけを表示する

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import random
-from settings import gacha_probs
+from gacha import gacha_draw
+
 
 def gacha_draw():
-    items = list(gacha_probs.keys())
-    weights = list(gacha_probs.values())
-    result = random.choices(items, weights=weights, k=1)[0]
-    return result
 
-if __name__ == "__main__":
-    result = gacha_draw()
-    print(f"ガチャの結果: {result}")
+    if __name__ == "__main__":
+        result = gacha_draw()
+        print(f"ガチャの結果: {result}")
