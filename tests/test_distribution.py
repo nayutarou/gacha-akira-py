5e6d66cf0e7b463d269ace71aad62a15212835ba
akira <a.oono.sys23@morijyobi.ac.jp>
2025-07-09 10:11:09 +0900

    テストの結果を出力

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -10,17 +10,30 @@ from collections import Counter
 
 
 def test_all_items_appear():
+    print("\n[分布テスト] すべてのアイテムが出現する")
     results = set()
-    for _ in range(10000):
-        results.add(gacha_draw())
-    assert set(gacha_probs.keys()) == results
+    try:
+        for _ in range(10000):
+            results.add(gacha_draw())
+        assert set(gacha_probs.keys()) == results
+        print("[テスト成功] すべてのアイテムが出現します")
+    except AssertionError:
+        print("[テスト失敗] すべてのアイテムが出現しません")
+        raise
 
 
 def test_distribution():
+    print("\n[分布テスト] 確率分布が正しい")
     N = 100000
     results = [gacha_draw() for _ in range(N)]
     counter = Counter(results)
-    for item, prob in gacha_probs.items():
-        observed = counter[item] / N * 100
-        # 許容誤差±1.5%
-        assert abs(observed - prob) < 1.5
+    try :
+        for item, prob in gacha_probs.items():
+            observed = counter[item] / N * 100
+            # 許容誤差±1.5%
+            assert abs(observed - prob) < 1.5
+            print("[テスト成功] 確率分布が正しく出現します")
+    except AssertionError:
+        print("[テスト失敗] 確率分布が正しく出現しません")
+        raise
+        
